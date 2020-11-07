812221037d7a0b4a7dd7ecac37183f61d81fb421
broly007 <kumarhritik158@gmail.com>
2020-11-08 01:36:13 +0530

    installed react-route-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import  HomePage from './pages/homepage/homepage.component'
+import {Route} from 'react-router-dom'
+import './App.css'
+ 
+const HatsPage = () =>(
+  <div>
+    <h1>HATS  PAGE </h1>
+  </div>
+)
+
 function App() {
   return (
-    <div> <HomePage/> </div>
+    <div> 
+      <Route  exact path='/'   component={HomePage}/>
+      <Route   path='/hat'   component={HatsPage}/>
+   </div>
   );
 }
 
